e06f404f2736ea76316a2d57701524599eeea64c
nnnnnnniam1 <nnnnnnniam1@gmail.com>
2024-01-10 13:29:19 +0900

    jdbc 코드 및 jdbc SpringConfig 변경

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,27 @@
 package hello.hellospring;
 
+import hello.hellospring.Repository.JdbcMemberRepository;
 import hello.hellospring.Repository.MemberRepository;
 import hello.hellospring.Repository.MemoryMemberRepository;
 import hello.hellospring.Service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+
+    // 바로 autowaired하거나 생성자를 통해서 활용
+    // @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){ this.dataSource = dataSource;}
+
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -15,7 +29,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+//        return new MemoryMemberRepository();      // MemMoryRepository
+        return new JdbcMemberRepository(dataSource);  // JdbcRepository 사용
     }
 
 }
