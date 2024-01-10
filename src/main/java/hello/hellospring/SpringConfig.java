7901a9f13b0947b52c7d6691f8fef74368e84c4c
nnnnnnniam1 <nnnnnnniam1@gmail.com>
2024-01-10 15:28:06 +0900

    SpringDataJpa

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -2,13 +2,9 @@ package hello.hellospring;
 
 import hello.hellospring.Repository.*;
 import hello.hellospring.Service.MemberService;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
@@ -16,7 +12,7 @@ public class SpringConfig {
 
     // 바로 autowaired하거나 생성자를 통해서 활용
     // @Autowired
-    private DataSource dataSource;
+//    private DataSource dataSource;
 
 //    @Autowired
 //    public SpringConfig(DataSource dataSource){ this.dataSource = dataSource;}
@@ -27,22 +23,37 @@ public class SpringConfig {
     * private EntityManager em;
     * */
 
-    private EntityManager em;
+//    private EntityManager em;
+//    @Autowired
+//    public SpringConfig(EntityManager em){this.em = em;}
+
+
+    // SpringDataJpa 사용 시
+    private final MemberRepository memberRepository;
     @Autowired
-    public SpringConfig(EntityManager em){this.em = em;}
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
+//    @Bean
+//    public MemberService memberService(){
+//        return new MemberService(memberRepository());
+//    }
 
+    /*
     @Bean
     public MemberRepository memberRepository(){
 
 //        return new MemoryMemberRepository();      // MemMoryRepository
 //        return new JdbcMemberRepository(dataSource);  // JdbcRepository
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+//        return new JpaMemberRepository(em);
+
     }
+     */
 
 }
--- src/test/java/hello/hellospring/Repository/SpringDataJpaMemberRepository.java
+++
@@ -1,2 +0,0 @@
-package hello.hellospring.Repository;public interface SpringDataJpaMemberRepository {
-}
