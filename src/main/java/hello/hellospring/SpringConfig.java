6f5ef1af091e57adba98b864b94d9793d0c9f0b8
nnnnnnniam1 <nnnnnnniam1@gmail.com>
2024-01-10 15:13:46 +0900

    jpa

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,10 +1,9 @@
 package hello.hellospring;
 
-import hello.hellospring.Repository.JdbcMemberRepository;
-import hello.hellospring.Repository.JdbcTemplateMemberRepository;
-import hello.hellospring.Repository.MemberRepository;
-import hello.hellospring.Repository.MemoryMemberRepository;
+import hello.hellospring.Repository.*;
 import hello.hellospring.Service.MemberService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,9 +18,18 @@ public class SpringConfig {
     // @Autowired
     private DataSource dataSource;
 
-    @Autowired
-    public SpringConfig(DataSource dataSource){ this.dataSource = dataSource;}
+//    @Autowired
+//    public SpringConfig(DataSource dataSource){ this.dataSource = dataSource;}
 
+    /*
+    * 원래 스펙에서는 이렇게 받아와야함
+    * @PersistenceContext
+    * private EntityManager em;
+    * */
+
+    private EntityManager em;
+    @Autowired
+    public SpringConfig(EntityManager em){this.em = em;}
 
     @Bean
     public MemberService memberService(){
@@ -33,7 +41,8 @@ public class SpringConfig {
 
 //        return new MemoryMemberRepository();      // MemMoryRepository
 //        return new JdbcMemberRepository(dataSource);  // JdbcRepository
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 
 }
